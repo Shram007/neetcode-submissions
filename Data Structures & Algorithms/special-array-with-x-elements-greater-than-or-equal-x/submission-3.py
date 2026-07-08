cae334664da0c5e912ea20b7678e54779b96aab6
Shram Kadia <65490105+Shram007@users.noreply.github.com>
2026-07-07 23:18:09 -0700

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-3

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        # Binary Search
+        l, r = 1, len(nums)
+        while l <= r:
+            mid = (l + r) // 2
+            cnt = sum(1 for num in nums if num >= mid)
+            if cnt == mid: return mid
+
+            if cnt < mid: r = mid - 1
+            else: l = mid + 1
+        return -1
