b62f8f5726a0d39a10d4fafd3acc2d1336f212a8
Shram Kadia <65490105+Shram007@users.noreply.github.com>
2026-07-09 22:47:18 -0700

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height: return 0
+        res = 0
+        stack = []
+        for i in range(len(height)):
+            while stack and height[i] >= height[stack[-1]]: 
+                mid = height[stack.pop()]
+                if stack:
+                    right = height[i]
+                    left = height[stack[-1]]
+                    h = min(left, right) - mid
+                    w = i - stack[-1] - 1
+                    res += h * w
+            stack.append(i)
+        return res
